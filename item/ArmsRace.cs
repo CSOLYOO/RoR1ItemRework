3de22a4384858325ef4f8af828303150f4af289b
nethercrowcsolyoo@gmail.com <nethercrowcsolyoo@gmail.com>
2020-04-19 18:45:24 +0800

    update description

--- item/ArmsRace.cs
+++ item/ArmsRace.cs
@@ -81,11 +81,11 @@ namespace RoR1ItemRework
             {
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_NAME_TOKEN", "Arms Race");
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_PICKUP_TOKEN", "Drones are equipped with explosive weaponry.");
-                R2API.AssetPlus.Languages.AddToken("ARMSRACE_DESCRIPTION_TOKEN", "On drone action: 9% chance for drones to fire missiles and mortars,deals 300%<style=cStack>(300% per stack)</style>damage.");
+                R2API.AssetPlus.Languages.AddToken("ARMSRACE_DESCRIPTION_TOKEN", "On drone action: 9% chance for drones and turrets to fire missiles and mortars,deals <style=cisDamage>300%</style><style=cStack>(300% per stack)</style>damage.");
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_LORE_TOKEN", "Drones are equipped with explosive weaponry.");
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_NAME_TOKEN", "军备竞赛", "zh-CN");
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_PICKUP_TOKEN", "无人机攻击有概率发射导弹", "zh-CN");
-                R2API.AssetPlus.Languages.AddToken("ARMSRACE_DESCRIPTION_TOKEN", "无人机有9%概率发射导弹，造成300%<style=cStack>（每层增加300%）</style>的伤害。", "zh-CN");
+                R2API.AssetPlus.Languages.AddToken("ARMSRACE_DESCRIPTION_TOKEN", "无人机有9%概率发射导弹，造成<style=cisDamage>300%</style><style=cStack>（每层增加300%）</style>的伤害。", "zh-CN");
                 R2API.AssetPlus.Languages.AddToken("ARMSRACE_LORE_TOKEN", "无人机被爆炸性武器武装了起来。", "zh-CN");
 
                 ItemDef ArmsRaceDef = new ItemDef
