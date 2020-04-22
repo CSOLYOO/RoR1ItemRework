f99ec1e9cc1803365de29efbdcdf332976c9d7df
nethercrowcsolyoo@gmail.com <nethercrowcsolyoo@gmail.com>
2020-04-22 15:18:40 +0800

    fix crit bug

--- item/ClassicCritThing.cs
+++ item/ClassicCritThing.cs
@@ -11,28 +11,27 @@ namespace RoR1ItemRework.item
     {
         public static void CritInit()
         {
+            On.RoR2.CharacterBody.RecalculateStats += OnCritAdd;
+        }
 
-
-            On.RoR2.CharacterBody.RecalculateStats += (On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self) =>
-           {
-               orig(self);
-               CharacterBody CB = self.GetComponent<CharacterBody>();
-               CharacterMaster Master = CB.master;
-               int count1 = Master.inventory.GetItemCount(ItemIndex.AttackSpeedOnCrit);
-               int count2 = Master.inventory.GetItemCount(ItemIndex.HealOnCrit);
-               float CritIncrement = 0f;
-               if (count1 > 0)
-               {
-                   CritIncrement += 5f * (count1 - 1);
-               }
-               if (count2 > 0)
-               {
-                   CritIncrement += 5f * (count2 - 1);
-               }
-               Reflection.SetPropertyValue(self, "crit", self.crit + CritIncrement);
-           };
-
-
+        private static void OnCritAdd(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+            if(self && self.inventory)
+            {
+                int count1 = self.inventory.GetItemCount(ItemIndex.AttackSpeedOnCrit);
+                int count2 = self.inventory.GetItemCount(ItemIndex.HealOnCrit);
+                float CritIncrement = 0f;
+                if (count1 > 0)
+                {
+                    CritIncrement += 5f * (count1 - 1);
+                }
+                if (count2 > 0)
+                {
+                    CritIncrement += 5f * (count2 - 1);
+                }
+                Reflection.SetPropertyValue(self, "crit", self.crit + CritIncrement);
+            }
         }
     }
 }
